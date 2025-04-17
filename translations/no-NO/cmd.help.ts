c1665c37625f0076f092776bafef07afc7e2cb3e
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-17 10:06:52 -0400

    New translations cmd.help.ts (Norwegian)

--- translations/no-NO/cmd.help.ts
+++ translations/no-NO/cmd.help.ts
@@ -2,7 +2,8 @@ module.exports = {
   main: {
     name: "Meny",
     shortDesc: "Tilbake til hovedmenyen.",
-    desc: "{info} • **About**\n> Begynn å integrere dine samfunn til Discord. Det er enkelt å Disping.\n\n**{award} • Credits**\n{allCredits}\n...Og alt vårt [\\`translators\\`](https://cdn.ch1ll.dev/r/translators. ng)!\n\n**{robot} • Ch1lDev's Andre boter**\n{allBots}\n\n**{discord} • Delta i vårt fellesskap! *\n> Du kan gjerne bli med i vårt [\\`Discord server\\`]({support}) for å støtte eller snakke med samfunnet."
+    desc: "{info} • **About**\n> Begynn å integrere dine samfunn til Discord. Det er enkelt å Disping.\n\n**{award} • Credits**\n{allCredits}\n...Og alt vårt [\\`translators\\`](https://cdn.ch1ll.dev/r/translators. ng)!\n\n**{robot} • Ch1lDev's Andre boter**\n{allBots}\n\n**{discord} • Delta i vårt fellesskap! *\n> Du kan gjerne bli med i vårt [\\`Discord server\\`]({support}) for å støtte eller snakke med samfunnet.",
+    footer: "Designed by Jake McQuade"
   },
   feedback: {
     name: "Tilbakemelding",
