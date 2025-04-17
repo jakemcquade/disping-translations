94a05fe1ae26a20ae4b1fc91477bcfd88b398bfd
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-17 10:06:50 -0400

    New translations cmd.help.ts (German)

--- translations/de-DE/cmd.help.ts
+++ translations/de-DE/cmd.help.ts
@@ -2,7 +2,8 @@ module.exports = {
   main: {
     name: "Menü",
     shortDesc: "Zurück zum Hauptmenü.",
-    desc: "{info} • **Info**\n> Integriere deine sozialen Netzwerke in Discord, auf einfache Art und Weise. Das ist Disping.\n\n**{award} • Anerkennung**\n{allCredits}\n...Und alle unsere [`Übersetzer`](https://cdn.ch1ll.dev/r/translators.png)!\n\n**{robot} • Ch1llDev's andere Bots**\n{allBots}\n\n**{discord} • Trete unserer Community bei!**\n> Gerne kannst du unserem [`Discord server`]({support}) für Unterstützung beitreten oder mit der Community reden."
+    desc: "{info} • **Info**\n> Integriere deine sozialen Netzwerke in Discord, auf einfache Art und Weise. Das ist Disping.\n\n**{award} • Anerkennung**\n{allCredits}\n...Und alle unsere [`Übersetzer`](https://cdn.ch1ll.dev/r/translators.png)!\n\n**{robot} • Ch1llDev's andere Bots**\n{allBots}\n\n**{discord} • Trete unserer Community bei!**\n> Gerne kannst du unserem [`Discord server`]({support}) für Unterstützung beitreten oder mit der Community reden.",
+    footer: "Designed by Jake McQuade"
   },
   feedback: {
     name: "Feedback",
