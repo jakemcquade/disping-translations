8c928895fed9a6f246362aef9f44677db77e442b
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-17 10:06:53 -0400

    New translations cmd.help.ts (Polish)

--- translations/pl-PL/cmd.help.ts
+++ translations/pl-PL/cmd.help.ts
@@ -2,7 +2,8 @@ module.exports = {
   main: {
     name: "Menu",
     shortDesc: "Wróć do menu głównego.",
-    desc: "{info} • **O **\n> Zacznij integrować swoje media społecznościowe z Discordem w łatwy sposób. To jest Disping.\n\n**{award} • Credits**\n{allCredits}\n...I wszystkich naszych [\\`tłumaczów\\`](https://cdn.ch1ll.dev/r/translators. ng)!\n\n**{robot} • Inne Boty Ch1llDev**\n{allBots}\n\n**{discord} • Dołącz do naszej społeczności! *\n> Dołącz do naszego [\\`Serwera Discord\\`]({support}) o wsparcie lub porozmawiaj z naszą społecznością."
+    desc: "{info} • **O **\n> Zacznij integrować swoje media społecznościowe z Discordem w łatwy sposób. To jest Disping.\n\n**{award} • Credits**\n{allCredits}\n...I wszystkich naszych [\\`tłumaczów\\`](https://cdn.ch1ll.dev/r/translators. ng)!\n\n**{robot} • Inne Boty Ch1llDev**\n{allBots}\n\n**{discord} • Dołącz do naszej społeczności! *\n> Dołącz do naszego [\\`Serwera Discord\\`]({support}) o wsparcie lub porozmawiaj z naszą społecznością.",
+    footer: "Designed by Jake McQuade"
   },
   feedback: {
     name: "Opinia",
