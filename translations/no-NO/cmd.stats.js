55db3839c77bb1db87e854b4183aebe93d47dd78
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:38 -0400

    New translations cmd.stats.js (Norwegian)

--- translations/no-NO/cmd.stats.js
+++ translations/no-NO/cmd.stats.js
@@ -1,23 +1,23 @@
 module.exports = {
   stats: {
-    name: "Statistikker",
-    desc: "Se DisPings statestikker.",
+    name: 'Statistikker',
+    desc: 'Se DisPings statestikker.',
     server: {
-      name: "**Server**",
-      value: "{rocket} CPU: **{cpu}%**\\n{stats} Minne: **{memory}%**\\n{clock} Oppetid: **{uptime}**"
+      name: '**Server**',
+      value: '{rocket} CPU: **{cpu}%**\\n{stats} Minne: **{memory}%**\\n{clock} Oppetid: **{uptime}**',
     },
     app: {
-      name: "**Bot statestikker**",
-      value: "{globe} Servere: **{servers}**\\n{player} Brukere: **{users}**"
-    }
+      name: '**Bot statestikker**',
+      value: '{globe} Servere: **{servers}**\\n{player} Brukere: **{users}**',
+    },
   },
   feedback: {
-    name: "Tilbakemelding",
-    message: "Melding"
+    name: 'Tilbakemelding',
+    message: 'Melding',
   },
   buttons: {
-    invite: "Inviter",
-    website: "Nettsted",
-    feedback: "Tilbakemelding"
-  }
+    invite: 'Inviter',
+    website: 'Nettsted',
+    feedback: 'Tilbakemelding',
+  },
 };
