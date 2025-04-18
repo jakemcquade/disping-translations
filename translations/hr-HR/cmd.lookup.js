2ec4d5b586f0b8b5373107d5ce44f1d8171df7df
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:26 -0400

    New translations cmd.lookup.js (Croatian)

--- translations/hr-HR/cmd.lookup.js
+++ translations/hr-HR/cmd.lookup.js
@@ -1,4 +1,5 @@
 module.exports = {
-  loading: "Traženje korisničkog \"{username}\" na svim podržanim platformama. Ovaj proces može potrajati neko vrijeme. Molim te čekaj...",
-  failed: "Čini se da je korisnik \"{username}\" vrlo tajnovit, nisam dobio rezultate dok sam tražio ovog korisnika."
+  loading:
+    'Traženje korisničkog "{username}" na svim podržanim platformama. Ovaj proces može potrajati neko vrijeme. Molim te čekaj...',
+  failed: 'Čini se da je korisnik "{username}" vrlo tajnovit, nisam dobio rezultate dok sam tražio ovog korisnika.',
 };
