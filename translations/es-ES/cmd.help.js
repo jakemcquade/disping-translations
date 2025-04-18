d4642e506a80180bd95a0483640ee9d7f2e677d0
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:04 -0400

    New translations cmd.help.js (Spanish)

--- translations/es-ES/cmd.help.js
+++ translations/es-ES/cmd.help.js
@@ -1,17 +1,17 @@
 module.exports = {
   main: {
-    name: "Menú",
-    shortDesc: "Volver al menú principal.",
+    name: 'Menú',
+    shortDesc: 'Volver al menú principal.',
     desc: "{info} • **About**\n> Start integrating your socials to Discord. The easy way. That's Disping.\n\n**{award} • Credits**\n{allCredits}\n...And all our [`translators`](https://cdn.ch1ll.dev/r/translators.png)!\n\n**{robot} • Ch1llDev's Other Bots**\n{allBots}\n\n**{discord} • Join Our Community!**\n> Feel free to join our [`Discord server`]({support}) for support, or to talk to the community.",
-    footer: "Designed by Jake McQuade"
+    footer: 'Designed by Jake McQuade',
   },
   feedback: {
-    name: "Comentarios",
-    message: "Mensaje"
+    name: 'Comentarios',
+    message: 'Mensaje',
   },
   buttons: {
-    invite: "Invitar",
-    website: "Sitio Web",
-    feedback: "Comentarios"
-  }
+    invite: 'Invitar',
+    website: 'Sitio Web',
+    feedback: 'Comentarios',
+  },
 };
