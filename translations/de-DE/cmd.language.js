9f5dc62018878dc2a08c494b67ba3de8ba243552
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:13 -0400

    New translations cmd.language.js (German)

--- translations/de-DE/cmd.language.js
+++ translations/de-DE/cmd.language.js
@@ -1,3 +1,3 @@
 module.exports = {
-  reply: "{alert} • Sprache wurde auf **{language}** geändert. Vielen Dank für die Benutzung von Disping!"
+  reply: '{alert} • Sprache wurde auf **{language}** geändert. Vielen Dank für die Benutzung von Disping!',
 };
