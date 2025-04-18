a220c00dbf4039d790a8af4a27d25263d5cadc52
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:21 -0400

    New translations cmd.lookup.js (Italian)

--- translations/it-IT/cmd.lookup.js
+++ translations/it-IT/cmd.lookup.js
@@ -1,4 +1,6 @@
 module.exports = {
-  loading: "Ricerca utente \"{username}\" su tutte le piattaforme supportate. Questo processo potrebbe richiedere un certo tempo. Attendere...",
-  failed: "Sembra che l'utente \"{username}\" è molto riservato, non ho ottenuto alcun risultato durante la ricerca di questo utente."
+  loading:
+    'Ricerca utente "{username}" su tutte le piattaforme supportate. Questo processo potrebbe richiedere un certo tempo. Attendere...',
+  failed:
+    'Sembra che l\'utente "{username}" è molto riservato, non ho ottenuto alcun risultato durante la ricerca di questo utente.',
 };
