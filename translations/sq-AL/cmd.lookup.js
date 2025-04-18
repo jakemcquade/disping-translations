026303f01c440079f820d376884b82cc8a19c612
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:25 -0400

    New translations cmd.lookup.js (Albanian)

--- translations/sq-AL/cmd.lookup.js
+++ translations/sq-AL/cmd.lookup.js
@@ -1,4 +1,6 @@
 module.exports = {
-  loading: "Po kërkon përdoruesin \"{username}\" në të gjitha platformat e mbështetura. Ky proces mund të marrë pak kohë. Te lutem prit...",
-  failed: "Duket se përdoruesi \"{username}\" është shumë i fshehtë, nuk mora asnjë rezultat gjatë kërkimit të këtij përdoruesi."
+  loading:
+    'Po kërkon përdoruesin "{username}" në të gjitha platformat e mbështetura. Ky proces mund të marrë pak kohë. Te lutem prit...',
+  failed:
+    'Duket se përdoruesi "{username}" është shumë i fshehtë, nuk mora asnjë rezultat gjatë kërkimit të këtij përdoruesi.',
 };
