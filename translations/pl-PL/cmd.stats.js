a43a0344f0431ddee9cd910994672889f4625115
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:38 -0400

    New translations cmd.stats.js (Polish)

--- translations/pl-PL/cmd.stats.js
+++ translations/pl-PL/cmd.stats.js
@@ -1,23 +1,23 @@
 module.exports = {
   stats: {
-    name: "Statystyki",
+    name: 'Statystyki',
     desc: "Zobacz statystyki Disping'a.",
     server: {
-      name: "**Serwer**",
-      value: "{rocket} CPU: **{cpu}%**\\n{stats} Pamięć: **{memory}%**\\n{clock} Aktualność: **{uptime}**"
+      name: '**Serwer**',
+      value: '{rocket} CPU: **{cpu}%**\\n{stats} Pamięć: **{memory}%**\\n{clock} Aktualność: **{uptime}**',
     },
     app: {
-      name: "**Statystyki bota**",
-      value: "{globe} Serwerów: **{servers}**\\n{player} Użytkowników: **{users}**"
-    }
+      name: '**Statystyki bota**',
+      value: '{globe} Serwerów: **{servers}**\\n{player} Użytkowników: **{users}**',
+    },
   },
   feedback: {
-    name: "Opinia",
-    message: "Wiadomość"
+    name: 'Opinia',
+    message: 'Wiadomość',
   },
   buttons: {
-    invite: "Zaproś",
-    website: "Strona",
-    feedback: "Opinia"
-  }
+    invite: 'Zaproś',
+    website: 'Strona',
+    feedback: 'Opinia',
+  },
 };
