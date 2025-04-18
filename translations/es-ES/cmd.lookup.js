62f17942bc718c8c634449298b97ecb3e6ebfa4b
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:13:20 -0400

    New translations cmd.lookup.js (Spanish)

--- translations/es-ES/cmd.lookup.js
+++ translations/es-ES/cmd.lookup.js
@@ -1,4 +1,5 @@
 module.exports = {
-  loading: "Searching for user \"{username}\" on all supported platforms. This process may take some time. Please wait...",
-  failed: "It seems user \"{username}\" is very secretive, I got no results while searching for this user."
+  loading:
+    'Searching for user "{username}" on all supported platforms. This process may take some time. Please wait...',
+  failed: 'It seems user "{username}" is very secretive, I got no results while searching for this user.',
 };
