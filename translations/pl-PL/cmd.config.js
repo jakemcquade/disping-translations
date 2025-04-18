747b4189f3d00bb0cbfbd8ca26c091e91643921d
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 18:12:59 -0400

    New translations cmd.config.js (Polish)

--- translations/pl-PL/cmd.config.js
+++ translations/pl-PL/cmd.config.js
@@ -1,112 +1,116 @@
 module.exports = {
-  none: "Brak",
-  config: "Ustawienia",
-  this_channel: "Użyj bieżącego kanału",
-  updated: "{alert} • Konfiguracja zaktualizowana. Dziękujemy za korzystanie z Disping.",
-  limit: "Osiągnąłeś limit użytkowników, których możesz dodać, używając Disping. Ten limit jest dostępny, więc każdy użytkownik może cieszyć się tym samym doświadczeniem przy użyciu Disping. Dziękujemy za zrozumienie!",
+  none: 'Brak',
+  config: 'Ustawienia',
+  this_channel: 'Użyj bieżącego kanału',
+  updated: '{alert} • Konfiguracja zaktualizowana. Dziękujemy za korzystanie z Disping.',
+  limit:
+    'Osiągnąłeś limit użytkowników, których możesz dodać, używając Disping. Ten limit jest dostępny, więc każdy użytkownik może cieszyć się tym samym doświadczeniem przy użyciu Disping. Dziękujemy za zrozumienie!',
   404: "Użytkownik **{username}** nie może zostać znaleziony! Upewnij się, że wprowadziłeś nazwę użytkownika {social} (lub nazwę użytkownika dla YouTube'a), a nie nazwę wyświetlaną.",
-  invalidURL: "Nieprawidłowy link.",
-  saveError: "Wystąpił błąd podczas zapisywania. Proszę ponownie skonfigurować wartość, którą próbowałeś zmienić. Jeśli nadal masz ten błąd, skontaktuj się z [support]({support}). Przepraszamy za niedogodności.",
+  invalidURL: 'Nieprawidłowy link.',
+  saveError:
+    'Wystąpił błąd podczas zapisywania. Proszę ponownie skonfigurować wartość, którą próbowałeś zmienić. Jeśli nadal masz ten błąd, skontaktuj się z [support]({support}). Przepraszamy za niedogodności.',
   format: {
-    spotify: "Nieprawidłowy adres URL. Adresy Spotify są normalnie takie jak: **https://open.spotify.com/artist/ID_JEST_TUTAJ**.",
+    spotify:
+      'Nieprawidłowy adres URL. Adresy Spotify są normalnie takie jak: **https://open.spotify.com/artist/ID_JEST_TUTAJ**.',
     reddit: "Nieprawidłowy adres URL. URL Reddit'a są normalnie takie jak: **https://www.reddit.com/r/memes**.",
-    threads: "Nieprawidłowy adres URL. Adresy wątków są normalnie takie jak: **https://threads.net/@ch1lldev**"
+    threads: 'Nieprawidłowy adres URL. Adresy wątków są normalnie takie jak: **https://threads.net/@ch1lldev**',
   },
   loading: {
-    title: "Ładowanie...",
-    desc: "Dodawanie użytkowników i konfigurowanie Disping'u, aby ich oglądać. Jeśli utkniesz na tym ekranie, skontaktuj się z [support]({support}). Dziękujemy za korzystanie z Disping!"
+    title: 'Ładowanie...',
+    desc: "Dodawanie użytkowników i konfigurowanie Disping'u, aby ich oglądać. Jeśli utkniesz na tym ekranie, skontaktuj się z [support]({support}). Dziękujemy za korzystanie z Disping!",
   },
   perms: {
-    invalid: "{alert} • Nie masz uprawnień (MANAGE_GUILD) do zarządzania ustawieniami serwera dla Disping. Skontaktuj się z właścicielem serwera, aby poprosić o to uprawnienie."
+    invalid:
+      '{alert} • Nie masz uprawnień (MANAGE_GUILD) do zarządzania ustawieniami serwera dla Disping. Skontaktuj się z właścicielem serwera, aby poprosić o to uprawnienie.',
   },
   buttons: {
-    saveexit: "Zapisz i wyjdź",
-    back: "Powrót",
-    support: "Pomoc",
-    website: "Strona",
-    feedback: "Opinia",
-    data: "Exportuj Dane"
+    saveexit: 'Zapisz i wyjdź',
+    back: 'Powrót',
+    support: 'Pomoc',
+    website: 'Strona',
+    feedback: 'Opinia',
+    data: 'Exportuj Dane',
   },
   colors: {
-    red: "Czerwony",
-    orange: "Pomarańczowy",
-    yellow: "Żółty",
-    green: "Zielony",
-    blue: "Niebieski",
-    purple: "Fioletowy",
-    brown: "Brązowy",
-    black: "Czarny",
-    darkgrey: "Ciemnoszary",
-    white: "Biały"
+    red: 'Czerwony',
+    orange: 'Pomarańczowy',
+    yellow: 'Żółty',
+    green: 'Zielony',
+    blue: 'Niebieski',
+    purple: 'Fioletowy',
+    brown: 'Brązowy',
+    black: 'Czarny',
+    darkgrey: 'Ciemnoszary',
+    white: 'Biały',
   },
   feedback: {
-    name: "Opinia",
-    content: "Zawartość",
-    cooldown: "Ostatnio wysłałeś opinię! Proszę poczekać **{minutes} minut** i **{seconds} sekund**."
+    name: 'Opinia',
+    content: 'Zawartość',
+    cooldown: 'Ostatnio wysłałeś opinię! Proszę poczekać **{minutes} minut** i **{seconds} sekund**.',
   },
   socialSelect: {
-    title: "{folder} Social Select",
-    desc: "Select a social below to get started!\n\n**{warning} Warning**: Twitter is very unstable at the moment. It will improve as time goes on."
+    title: '{folder} Social Select',
+    desc: 'Select a social below to get started!\n\n**{warning} Warning**: Twitter is very unstable at the moment. It will improve as time goes on.',
   },
   socials: {
-    dropdown: "Wybierz użytkownika, którym chcesz zarządzać.",
+    dropdown: 'Wybierz użytkownika, którym chcesz zarządzać.',
     state: {
-      true: "Użyj",
-      false: "Nie używaj",
-      show: "Pokaż",
-      hide: "Ukryj",
-      create: "Utwórz",
-      noCreate: "Nie twórz",
-      include: "Zawiera",
-      exclude: "Wyklucz"
+      true: 'Użyj',
+      false: 'Nie używaj',
+      show: 'Pokaż',
+      hide: 'Ukryj',
+      create: 'Utwórz',
+      noCreate: 'Nie twórz',
+      include: 'Zawiera',
+      exclude: 'Wyklucz',
     },
     users: {
-      name: "Użytkownicy",
-      add: "Dodaj użytkownika",
-      none: "Nie dodano użytkowników... kliknij menu rozwijane poniżej i wybierz \"Dodaj użytkownika\", aby rozpocząć!\n\n✨ Pamiętaj, aby zapisać po dodaniu użytkownika.",
-      notify: "Powiadom",
-      url: "URL",
-      failed: "Konfiguracja nie powiodła się"
+      name: 'Użytkownicy',
+      add: 'Dodaj użytkownika',
+      none: 'Nie dodano użytkowników... kliknij menu rozwijane poniżej i wybierz "Dodaj użytkownika", aby rozpocząć!\n\n✨ Pamiętaj, aby zapisać po dodaniu użytkownika.',
+      notify: 'Powiadom',
+      url: 'URL',
+      failed: 'Konfiguracja nie powiodła się',
     },
     channel: {
-      name: "Kanał",
-      desc: "Wybierz kanał z listy rozwijanej poniżej.",
-      placeholder: "Wybierz kanał do użycia."
+      name: 'Kanał',
+      desc: 'Wybierz kanał z listy rozwijanej poniżej.',
+      placeholder: 'Wybierz kanał do użycia.',
     },
     role: {
-      name: "Rola",
-      desc: "Proszę wybrać rolę do wspomnień z listy rozwijanej poniżej.",
-      placeholder: "Wybierz rolę do użycia."
+      name: 'Rola',
+      desc: 'Proszę wybrać rolę do wspomnień z listy rozwijanej poniżej.',
+      placeholder: 'Wybierz rolę do użycia.',
     },
     color: {
-      name: "Kolor",
-      desc: "Wybierz kolor do pokazania na osadzeniu z listy rozwijanej poniżej.",
-      placeholder: "Wybierz kolor do zastosowania na osadzeniu."
+      name: 'Kolor',
+      desc: 'Wybierz kolor do pokazania na osadzeniu z listy rozwijanej poniżej.',
+      placeholder: 'Wybierz kolor do zastosowania na osadzeniu.',
     },
     message: {
-      name: "Wiadomość"
+      name: 'Wiadomość',
     },
     advanced: {
-      embed_editor: "Customizer Posta",
-      main: "> Witaj w edytorze posta. Naciśnij przycisk poniżej, aby włączyć/wyłączyć część osadzoną.",
-      embed: "Osadzenie",
-      author: "Autor",
-      thumbnail: "Miniaturka",
-      description: "Opis",
-      timestamp: "Czas",
-      test: "Wyślij post testowy",
-      event: "Wydarzenie na żywo",
-      crosspost: "Crosspost",
-      retweets: "Retweety",
-      useTFX: "TwitterFX",
-      createThread: "Thread",
+      embed_editor: 'Customizer Posta',
+      main: '> Witaj w edytorze posta. Naciśnij przycisk poniżej, aby włączyć/wyłączyć część osadzoną.',
+      embed: 'Osadzenie',
+      author: 'Autor',
+      thumbnail: 'Miniaturka',
+      description: 'Opis',
+      timestamp: 'Czas',
+      test: 'Wyślij post testowy',
+      event: 'Wydarzenie na żywo',
+      crosspost: 'Crosspost',
+      retweets: 'Retweety',
+      useTFX: 'TwitterFX',
+      createThread: 'Thread',
       button: {
-        youtube: "Przycisk obserwujący",
-        twitter: "Przycisk Podglądu",
-        twitch: "Przycisk oglądania",
-        reddit: "Przycisk Podglądu",
-        spotify: "Przycisk nasłuchiwania"
-      }
-    }
-  }
+        youtube: 'Przycisk obserwujący',
+        twitter: 'Przycisk Podglądu',
+        twitch: 'Przycisk oglądania',
+        reddit: 'Przycisk Podglądu',
+        spotify: 'Przycisk nasłuchiwania',
+      },
+    },
+  },
 };
