82b2a29c5dfe0aa420b058c51c28f5e1873fd00e
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 21:10:24 -0400

    Removed Old Translations

--- translations/no-NO/cmd.lookup.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  loading: "Søker etter bruker \"{username}\" på alle støttede plattformer. Denne prosessen kan ta litt tid. Vennligst vent...",
-  failed: "Det virker som brukeren \"{username}\" er svært hemmelighetsfull, jeg fikk ingen resultater mens jeg søkte etter denne brukeren."
-};
--- translations/sq-AL/cmd.lookup.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  loading: "Po kërkon përdoruesin \"{username}\" në të gjitha platformat e mbështetura. Ky proces mund të marrë pak kohë. Te lutem prit...",
-  failed: "Duket se përdoruesi \"{username}\" është shumë i fshehtë, nuk mora asnjë rezultat gjatë kërkimit të këtij përdoruesi."
-};
