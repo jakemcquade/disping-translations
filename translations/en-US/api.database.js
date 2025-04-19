c62224dae0483086ca6ee68efc41bbe2c2fd816f
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 21:10:57 -0400

    Added API Translations

--- translations/en-US/api.database.js
+++ translations/en-US/api.database.js
@@ -1,4 +1,19 @@
 module.exports = {
-    missing_access: "User missing access to server.",
-    
+    missing_access: "User missing permissions to access the server.",
+    ratelimit: "Platform is currently being ratelimited. This is an internal issue. Please report this.",
+    vote_required: "You must vote for Disping to use this feature. Please visit https://disping.xyz/vote to vote.",
+    test_cooldown: "You've already sent a test message recently! Please wait {cooldown} seconds.",
+    user: {
+        limit: "You have reached the maximum number of users you can add for this social. Buy Disping premium to add more.",
+        not_found: "User \"{username}\" does not exist.",
+        already_added: "User \"{username}\" has already been added."
+    },
+    channel: {
+        not_found: "Failed to find channel. Please make sure Disping can send messages and view the selected channel.",
+        no_channel: "No channel/webhook was added to this user.",
+    },
+    test: {
+        cooldown: "You have already sent a test message recently! Please wait {cooldown} seconds.",
+        failed:  "Failed to send test message. Please make sure Disping can send messages and view the selected channel."
+    }
 }
