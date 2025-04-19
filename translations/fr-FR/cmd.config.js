26e73e163923892c99e0a71bc4d866014bc2da9c
Jake McQuade <183949683+jakemcquade@users.noreply.github.com>
2025-04-18 21:13:43 -0400

    New translations cmd.config.js (French)

--- translations/fr-FR/cmd.config.js
+++ translations/fr-FR/cmd.config.js
@@ -1,36 +1,4 @@
 module.exports = {
-  none: 'Aucun',
-  config: 'Configuration',
-  this_channel: 'Use Current Channel',
-  updated: '{alert} • Configuration updated. Thank you for using Disping.',
-  limit:
-    'You hit the limit of users you can add using Disping. This limit is in place, so every user can enjoy the same experience using Disping. Thank you for understanding!',
-  404: "L'utilisateur **{username}** est introuvable! Veuillez vous assurer que vous avez bien entré le nom d'utilisateur {social} (ou nom d'utilisateur YouTube) et non le nom affiché sur l'écran.",
-  invalidURL: 'URL invalide.',
-  saveError:
-    "Une erreur est survenue lors de l'enregistrement. Veuillez reconfigurer la valeur que vous étiez en train d'essayer de modifier. Si cette erreur persiste, veuillez contactez [support]({support}). Excusez nous du désagrément.",
-  format: {
-    spotify:
-      'URL invalide. Les URLs Spotify ressemblent normalement à ceci: **https://open.spotify.com/artist/ID_ICI**.',
-    reddit: 'URL invalide. Les URLs Reddit ressemblent normalement à ceci: **https://www.reddit.com/r/memes**.',
-    threads: 'Invalid URL. Thread URLs are normally like this: **https://threads.net/@ch1lldev**',
-  },
-  loading: {
-    title: 'Chargement en cours...',
-    desc: 'Adding users and configuring Disping to watch them. If you get stuck on this screen, please contact [support]({support}). Thanks for using Disping!',
-  },
-  perms: {
-    invalid:
-      "{alert} • You do not have permission (MANAGE_GUILD) to manage this server's settings for Disping. Please contact the server owner to request this permission.",
-  },
-  buttons: {
-    saveexit: 'Save & Exit',
-    back: 'Back',
-    support: 'Assistance',
-    website: 'Site Web',
-    feedback: 'Avis',
-    data: 'Export Data',
-  },
   colors: {
     red: 'Rouge',
     orange: 'Orange',
